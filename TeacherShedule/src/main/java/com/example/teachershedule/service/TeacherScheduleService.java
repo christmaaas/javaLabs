4c62a04dc308db82204cae75b63b89ffe9272261
Artem <slinko.art@mail.ru>
2024-02-25 12:40:27 +0300

    service updated

--- TeacherShedule/src/main/java/com/example/teachershedule/service/TeacherScheduleService.java
+++ TeacherShedule/src/main/java/com/example/teachershedule/service/TeacherScheduleService.java
@@ -1,25 +1,27 @@
 package com.example.teachershedule.service;
 
-import com.example.teachershedule.dao.TeacherScheduleDao;
 import com.example.teachershedule.dto.ScheduleResponceDto;
 import com.example.teachershedule.entity.TeacherEntity;
 import com.example.teachershedule.dao.TeacherScheduleRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class TeacherScheduleService
 {
-    private final TeacherScheduleDao teacherScheduleDao;
     private final TeacherScheduleRepository teacherScheduleRepository;
 
-    public TeacherScheduleService(TeacherScheduleDao teacherScheduleDao, TeacherScheduleRepository teacherScheduleRepository) {
-        this.teacherScheduleDao = teacherScheduleDao;
+    public TeacherScheduleService(TeacherScheduleRepository teacherScheduleRepository) {
         this.teacherScheduleRepository = teacherScheduleRepository;
     }
 
     public ScheduleResponceDto searchTeacherSchedule(String teacherId)
     {
-        ScheduleResponceDto scheduleResponceDto = teacherScheduleDao.searchTeacherSchedule(teacherId);
+        String apiRequest = "https://iis.bsuir.by/api/v1/employees/schedule/" + teacherId;
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ScheduleResponceDto scheduleResponceDto = restTemplate.getForObject(apiRequest, ScheduleResponceDto.class);
 
         TeacherEntity teacher = new TeacherEntity();
         teacher.setId(scheduleResponceDto.getEmployeeDto().getId());
