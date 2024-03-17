3ac05d3b3de0546d52c3fa40248dcff7c4b7e824
Artem <slinko.art@mail.ru>
2024-03-17 23:02:24 +0300

    some changes for tests

---
+++ TeacherShedule/src/test/java/com/example/teachershedule/TeacherScheduleApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.teachershedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TeacherScheduleApplicationTest {
+
+    @Test
+    public void contextLoads() {
+        // Проверяем, что контекст приложения успешно загружается без ошибок
+    }
+
+}
+
